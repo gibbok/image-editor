cb1294594b869ce0222d0c1ec72ab410d65ab4ad
GibboK <gibbok.coding@gmail.com>
2023-03-09 16:21:24 +0100

    setup baseurl axios

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://picsum.photos/v2/';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
