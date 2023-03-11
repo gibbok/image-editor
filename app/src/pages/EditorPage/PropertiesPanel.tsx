7cee15fd7c4545b26eb9900e6caa82f1beef7c51
GibboK <gibbok.coding@gmail.com>
2023-03-11 22:58:31 +0100

    add basic form with validation

--- app/src/pages/EditorPage/PropertiesPanel.tsx
+++ app/src/pages/EditorPage/PropertiesPanel.tsx
@@ -1,11 +1,13 @@
 import {
   Box,
+  Button,
   Checkbox,
   FormControl,
   FormControlLabel,
+  Input,
   TextField,
 } from '@mui/material';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import React from 'react';
@@ -76,99 +78,75 @@ export const PropertiesPanel = ({
   });
 
   const {
-    register,
+    control,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>({
-    resolver: yupResolver(schema),
-  });
+  } = useForm<FormData>({ resolver: yupResolver(schema) });
 
   const onSubmit = (data: FormData) => {
     setImageProps((prevState) => ({ ...prevState, data }));
   };
 
-  const handleChangeWidth = (e: EventTextField) => {
-    setImageProps((prevState) => ({
-      ...prevState,
-      width: clamp(
-        parseInt(e.target.value, 10),
-        EDITOR_MIN_WIDTH,
-        EDITOR_MAX_WIDTH
-      ),
-    }));
-  };
-
-  console.log('xxx', imageProps);
-
   return (
     <Box mt={6}>
-      {/* <FormControl>
-        <TextField
-          fullWidth
-          required
-          label="Width"
-          type="number"
-          defaultValue={width}
-          value={imageProps.width}
-          InputProps={{
-            inputProps: {
-              min: EDITOR_MIN_WIDTH,
-              max: EDITOR_MAX_WIDTH,
-            },
-          }}
-          onChange={handleChangeWidth}
-        />
-        <TextField
-          fullWidth
-          required
-          id="height"
-          label="Height"
-          type="number"
-          value={imageProps.height}
-          InputProps={{
-            inputProps: {
-              min: EDITOR_MIN_WIDTH,
-              max: EDITOR_MAX_WIDTH,
-            },
-          }}
-        />
-        <FormControlLabel
-          control={<Checkbox checked={imageProps.isGrayscale} />}
-          label="Grayscale"
-        />
-        <TextField
-          fullWidth
-          required
-          id="blur"
-          label="Blur"
-          type="number"
-          value={imageProps.blur}
-          InputProps={{
-            inputProps: {
-              min: EDITOR_MIN_BLUR,
-              max: EDITOR_MAX_BLUR,
-            },
-          }}
-        />
-      </FormControl> */}
-      <form onSubmit={handleSubmit(onSubmit)}>
-        width
-        <input {...register('width')} defaultValue={width} />
-        <p>{errors.width?.message}</p>
-        height
-        <input {...register('height')} defaultValue={height} />
-        <p>{errors.height?.message}</p>
-        isGrayscale
-        <input
-          {...register('isGrayscale')}
-          defaultValue={isGrayscale.toString()}
-        />
-        <p>{errors.isGrayscale?.message}</p>
-        blur
-        <input {...register('blur')} defaultValue={blur} />
-        <p>{errors.blur?.message}</p>
-        <input type="submit" />
-      </form>
+      <FormControl>
+        <Box component="form" onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            name="width"
+            control={control}
+            render={({ field }) => (
+              <TextField
+                type="number"
+                label="Width"
+                {...field}
+                defaultValue={width}
+              />
+            )}
+          />
+          <p>{errors.width?.message}</p>
+          <Controller
+            name="height"
+            control={control}
+            render={({ field }) => (
+              <TextField
+                type="number"
+                label="Height"
+                {...field}
+                defaultValue={height}
+              />
+            )}
+          />
+          <p>{errors.height?.message}</p>
+          <Controller
+            name="isGrayscale"
+            control={control}
+            render={({ field }) => (
+              <FormControlLabel
+                control={<Checkbox checked={isGrayscale} {...field} />}
+                label="Grayscale"
+              />
+            )}
+          />
+          <p>{errors.isGrayscale?.message}</p>
+          <Controller
+            name="blur"
+            control={control}
+            render={({ field }) => (
+              <TextField
+                type="number"
+                label="Blur"
+                defaultValue={blur}
+                {...field}
+              />
+            )}
+          />
+          <p>{errors.blur?.message}</p>
+          <Button type="submit" variant="outlined">
+            Apply
+          </Button>
+          {/* <input type="submit" /> */}
+        </Box>
+      </FormControl>
     </Box>
   );
 };
