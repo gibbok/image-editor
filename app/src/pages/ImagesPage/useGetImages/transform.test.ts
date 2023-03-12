72a941449c33a77ce656dfcedc7fdfea714e34dc
GibboK <gibbok.coding@gmail.com>
2023-03-12 23:38:04 +0100

    add tests

--- app/src/pages/ImagesPage/useGetImages/transform.test.ts
+++ app/src/pages/ImagesPage/useGetImages/transform.test.ts
@@ -1,27 +1,8 @@
 import { Images } from '../../../types-api';
 import { ImagesUI, PaginationMoveState } from '../../../types-ui';
-import {
-  modifySizeForImageUrl,
-  calculateImageSizesAspectRatioFitImage,
-  roundImageSizes,
-  extractImageSizesFromUrl,
-  makeUrlWithFitImageSizes,
-} from '../../../utils-urls';
 import { getPaginationInfoFromHeader, tranformResponseForUI } from './tranform';
-import { ImageSizes } from './type';
 
 describe('transform', () => {
-  describe('modifySizeForImageUrl', () => {
-    it('should modify url with given width and height', () => {
-      expect(
-        modifySizeForImageUrl('https://picsum.photos/id/3/5000/3333')({
-          width: 200,
-          height: 100,
-        })
-      ).toBe('https://picsum.photos/id/3/200/100');
-    });
-  });
-
   describe('getPaginationInfoFromHeader', () => {
     const cases: ReadonlyArray<[string, PaginationMoveState]> = [
       ['<https://picsum.photos/v2/list?page=2&limit=30>; rel="next"', 'next'],
@@ -78,67 +59,6 @@ describe('transform', () => {
           urlTransform: 'https://picsum.photos/id/103/100/75',
         },
       ]);
-    });
-  });
-
-  describe('calculateImageSizesAspectRatioFitImage', () => {
-    it('should resize maintaining aspec ratio', () => {
-      const originalAspectRate = 500 / 280;
-      const result = calculateImageSizesAspectRatioFitImage({
-        width: 200,
-        height: 160,
-      })({ width: 500, height: 280 });
-
-      expect(result).toEqual<ImageSizes>({
-        width: 200,
-        height: 112,
-      });
-      expect(result.width / result.height).toBe(originalAspectRate);
-    });
-  });
-
-  describe('roundImageSizes', () => {
-    it('should round image sizes', () => {
-      expect(roundImageSizes({ width: 100.2, height: 80.8 })).toEqual({
-        width: 100,
-        height: 81,
-      });
-    });
-  });
-
-  describe('extractImageSizesFromUrl', () => {
-    it('should extract width and height from url', () => {
-      // TODO add test unhappy path
-      expect(
-        extractImageSizesFromUrl('https://picsum.photos/id/103/2592/1936')
-      ).toEqual<ImageSizes>({
-        width: 2592,
-        height: 1936,
-      });
-    });
-  });
-
-  describe('makeUrlWithFitImageSizes', () => {
-    it('should return an url which fit the desired image sizes maintaining the original aspect ration', () => {
-      expect(
-        makeUrlWithFitImageSizes({
-          originalUrl: 'https://picsum.photos/id/103/2592/1936',
-          desiredSizes: {
-            width: 200,
-            height: 160,
-          },
-        })
-      ).toBe('https://picsum.photos/id/103/200/149');
-
-      expect(
-        makeUrlWithFitImageSizes({
-          originalUrl: 'https://picsum.photos/id/103/2592/1936',
-          desiredSizes: {
-            width: 200,
-            height: 160,
-          },
-        })
-      ).toBe('https://picsum.photos/id/103/200/149');
     });
   });
 });
--- app/src/pages/ImagesPage/utils.test.ts
+++ app/src/pages/ImagesPage/utils.test.ts
@@ -1,7 +1,11 @@
-import { makeEditorUrl, makeImagesPageQueryParams } from './utils';
+import {
+  getImagesPageQueryParams,
+  makeEditorUrl,
+  makeImagesPageQueryParams,
+} from './utils';
 
 describe('utils', () => {
-  describe('makeEdmakeEditorUrlitorUrl', () => {
+  describe('makeEditorUrl', () => {
     it('should return editor url', () => {
       expect(makeEditorUrl('125', 1)).toBe(
         'editor?imageId=125&page=1&width=800&height=600&grayscale=false&blur=1'
@@ -12,6 +16,13 @@ describe('utils', () => {
   describe('makeImagesPageQueryParams', () => {
     it('should return query parameter for page', () => {
       expect(makeImagesPageQueryParams(10)).toBe('?page=10');
+    });
+  });
+
+  describe('getImagesPageQueryParams', () => {
+    it('should return query parameter for page', () => {
+      const url = new URL('http://localhost:3000/?page=2');
+      expect(getImagesPageQueryParams(url.searchParams)).toEqual({ page: 2 });
     });
   });
 });
